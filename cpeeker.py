00494d721a7576d6ca09615dd8f43eca399c56f8
aFunnyBit <19717159+aFunnyBit@users.noreply.github.com>
2017-12-23 00:45:32 -0600

    added pretty print for returning results

---
+++ cpeeker.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+import sys
+import requests
+import json
+
+request = requests.get('https://api.coinmarketcap.com/v1/ticker/')
+coin_dict = request.json()
+coin_id = str(sys.argv[1])
+results = 0
+
+for x in coin_dict:
+    if (x["id"] == coin_id or x['symbol'] == coin_id.upper()):
+        results = json.dumps(x,indent=4)
+        break
+
+print(results)
+
+
